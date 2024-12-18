131200737df5f992f3c077fb70eac4262968f68e
Maks <makzylinski@gmail.com>
2024-12-18 20:11:39 +0100

    added dummy components

---
+++ connext/src/app/components/main-swipe/main-swipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-swipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-swipe.component.html',
+  styleUrl: './main-swipe.component.scss'
+})
+export class MainSwipeComponent {
+
+}
