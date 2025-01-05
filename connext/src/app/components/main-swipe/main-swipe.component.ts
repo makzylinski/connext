4ed3d03cfd22d451689c776418863d9ce1afb398
Maks <makzylinski@gmail.com>
2025-01-05 18:21:04 +0100

    feat: add MatIconModule to main-swipe component for enhanced icon support

--- connext/src/app/components/main-swipe/main-swipe.component.ts
+++ connext/src/app/components/main-swipe/main-swipe.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MainSwipeService } from '../../services/main-swipe.service';
 
 @Component({
   selector: 'app-main-swipe',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconModule],
   standalone: true,
   templateUrl: './main-swipe.component.html',
   styleUrl: './main-swipe.component.scss',
