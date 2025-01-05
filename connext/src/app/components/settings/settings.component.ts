50fa64a2b1d82a091ee87024c4fcbccba15612c8
Maks <makzylinski@gmail.com>
2025-01-05 19:19:42 +0100

    feat: enhance settings component with Material design card and icon

--- connext/src/app/components/settings/settings.component.ts
+++ connext/src/app/components/settings/settings.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
