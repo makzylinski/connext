0f919edd341077817025d690113c6da778eb835a
Maks <makzylinski@gmail.com>
2025-01-05 18:49:33 +0100

    feat: enhance toolbar with login/logout functionality and tooltips

--- connext/src/app/components/toolbar/toolbar.component.ts
+++ connext/src/app/components/toolbar/toolbar.component.ts
@@ -3,11 +3,23 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [MatToolbarModule, MatIconModule, MatMenuModule, MatDividerModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatTooltipModule,
+  ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  isLoggedIn: boolean = this.authService.isLoggedIn();
+
+  constructor(private readonly authService: AuthService) {}
+}
