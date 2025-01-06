f47980624e3d2479695c4bbc486acb64aa568db1
Maks <makzylinski@gmail.com>
2025-01-06 15:12:48 +0100

    feat: implement dynamic settings display with Material design and add settings model

--- connext/src/app/components/settings/settings.component.ts
+++ connext/src/app/components/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { Settings } from '../../models/settings.model';
 
 @Component({
   selector: 'app-settings',
@@ -9,4 +10,19 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './settings.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SettingsComponent {}
+export class SettingsComponent {
+  settings: Settings[] = [
+    {
+      icon: 'person',
+      title: 'Account Settings',
+    },
+    {
+      icon: 'settings',
+      title: 'App Settings',
+    },
+    {
+      icon: 'logout',
+      title: 'Logout',
+    },
+  ];
+}
---
+++ connext/src/app/models/settings.model.ts
@@ -0,0 +1,4 @@
+export interface Settings {
+  icon: string;
+  title: string;
+}
