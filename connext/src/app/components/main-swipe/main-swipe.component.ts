d2e86bc045156c76719f57bf967f3969e76975eb
Maks <makzylinski@gmail.com>
2025-01-07 17:04:43 +0100

    feat: implement profile card component with Material design styling and actions

--- connext/src/app/components/main-swipe/main-swipe.component.ts
+++ connext/src/app/components/main-swipe/main-swipe.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MainSwipeService } from '../../services/main-swipe.service';
 
 @Component({
   selector: 'app-main-swipe',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   standalone: true,
   templateUrl: './main-swipe.component.html',
   styleUrl: './main-swipe.component.scss',
