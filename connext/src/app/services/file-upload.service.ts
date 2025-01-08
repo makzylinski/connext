abe25c03b33c85d410bcadfcc06b358529544ca8
Maks <makzylinski@gmail.com>
2025-01-08 12:21:22 +0100

    feat: add photo upload component and service for profile image uploads

---
+++ connext/src/app/services/file-upload.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FileUploadService {
+  private readonly baseUrl = environment.baseUrl;
+
+  constructor(private readonly http: HttpClient) {}
+
+  uploadProfileImage = (userId: number, file: File) => {
+    const formData: FormData = new FormData();
+    formData.append('userId', userId.toString());
+    formData.append('file', file);
+
+    return this.http.post<string>(
+      `${this.baseUrl}/uploadProfileImage`,
+      formData
+    );
+  };
+}
