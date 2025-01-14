6bdf8c2d4677a626d56b2c1789729841334427c7
Maks <makzylinski@gmail.com>
2025-01-14 18:04:31 +0100

    feat: add photo upload functionality to account settings and update image fetching logic

--- connext/src/app/components/settings/account-settings/account-settings.component.ts
+++ connext/src/app/components/settings/account-settings/account-settings.component.ts
@@ -1,10 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FileUploadService } from '../../../services/file-upload.service';
+import { PhotoUploadComponent } from '../../photo-upload/photo-upload.component';
 
 @Component({
   selector: 'app-account-settings',
-  imports: [],
+  imports: [PhotoUploadComponent, CommonModule],
   templateUrl: './account-settings.component.html',
   styleUrl: './account-settings.component.scss',
+  standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AccountSettingsComponent {}
+export class AccountSettingsComponent {
+  imgSrc$?: Observable<string>;
+  constructor(private readonly fileUploadService: FileUploadService) {}
+
+  fetchImage = () =>
+    (this.imgSrc$ = this.fileUploadService.fetchProfileImage());
+}
