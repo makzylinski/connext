7dbd5f7cd49f6ab8fcf1f7a77fbc80ccb28bd5a4
Maks <makzylinski@gmail.com>
2025-01-16 14:22:35 +0100

    feat: implement bio input field with Material design and save button

--- connext/src/app/components/settings/bio/bio.component.ts
+++ connext/src/app/components/settings/bio/bio.component.ts
@@ -1,12 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-bio',
-  imports: [],
+  imports: [FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './bio.component.html',
   styleUrl: './bio.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BioComponent {
-
+  bioValue: string;
 }
