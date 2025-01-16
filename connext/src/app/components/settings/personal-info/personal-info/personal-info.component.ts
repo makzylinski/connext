b9a8f9294183967012c1602516b1dc96f8307798
Maks <makzylinski@gmail.com>
2025-01-16 19:27:15 +0100

    added new component

---
+++ connext/src/app/components/settings/personal-info/personal-info/personal-info.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-info',
+  imports: [],
+  templateUrl: './personal-info.component.html',
+  styleUrl: './personal-info.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PersonalInfoComponent {
+
+}
