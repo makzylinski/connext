c1274ff28cfcfb0828db83a7df2302dbae14218d
Maks <makzylinski@gmail.com>
2025-03-26 15:19:42 +0100

    feat: implement MatchesThumbnails component with profile images display

--- connext/src/app/components/profile/matches-thumbnails/matches-thumbnails/matches-thumbnails.component.ts
+++ connext/src/app/components/profile/matches-thumbnails/matches-thumbnails/matches-thumbnails.component.ts
@@ -8,4 +8,17 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class MatchesThumbnailsComponent {}
+export class MatchesThumbnailsComponent {
+  testProfiles = [
+    {
+      image:
+        'https://www.man-shop.eu/thumbnail/db/93/05/1726129673/MAN%20Lifestyle%20Merchandising%20Shop%20Bekleidung%20Herren_800x800.png',
+      name: 'John Doe',
+    },
+    {
+      image:
+        'https://as2.ftcdn.net/v2/jpg/02/95/69/87/1000_F_295698768_8NS9qp5sJmOMCEQDbcpGTZd7DttKnAql.jpg',
+      name: 'Jane Doe',
+    },
+  ];
+}
