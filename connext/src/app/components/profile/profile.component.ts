bf0ba7f4d3ebaa4b93570442f26525868516fa82
Maks <makzylinski@gmail.com>
2025-03-26 16:08:51 +0100

    feat: integrate Messages component into Profile view and update styles

--- connext/src/app/components/profile/profile.component.ts
+++ connext/src/app/components/profile/profile.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ChatListComponent } from './chat-list/chat-list/chat-list.component';
 import { MatchesThumbnailsComponent } from './matches-thumbnails/matches-thumbnails/matches-thumbnails.component';
+import { MessagesComponent } from './messages/messages/messages.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatchesThumbnailsComponent, ChatListComponent],
+  imports: [MatchesThumbnailsComponent, ChatListComponent, MessagesComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
