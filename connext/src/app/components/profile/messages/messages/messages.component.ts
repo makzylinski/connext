567c24f74f3fba4f1a219a028021bfd36af8afb1
Maks <makzylinski@gmail.com>
2025-03-27 19:33:39 +0100

    feat: implement Messages component with message display and styling

--- connext/src/app/components/profile/messages/messages/messages.component.ts
+++ connext/src/app/components/profile/messages/messages/messages.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Message } from '../../../../models/message.interface';
 
 @Component({
   selector: 'app-messages',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesComponent {
-
+  messages: Message[] = [
+    {
+      id: 1,
+      message: 'Hello, how are you?',
+      sender: 'John Doe',
+      date: '2021-01-01',
+      fromMe: true,
+    },
+    {
+      id: 2,
+      message: 'I am fine, thank you!',
+      sender: 'Jane Doe',
+      date: '2021-01-02',
+      fromMe: false,
+    },
+  ];
 }
