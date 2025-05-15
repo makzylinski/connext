7d24e9fab58defd83da076ec71a6ce6cb11afbe2
Maks <makzylinski@gmail.com>
2025-05-15 16:47:55 +0200

    fix: update chat-list component to use pairs$ observable for data binding

--- src/app/components/profile/chat-list/chat-list/chat-list.component.ts
+++ src/app/components/profile/chat-list/chat-list/chat-list.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../../../models/user.model';
 
 @Component({
   selector: 'app-chat-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.scss',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChatListComponent {
-  @Input() chats: any[];
+  @Input() pairs$: Observable<User[]>;
 }
