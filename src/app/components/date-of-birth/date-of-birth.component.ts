977651e0fbe86d044f2f2411041ce07c1609f241
Maks <makzylinski@gmail.com>
2025-06-03 00:06:45 +0200

    added datapicker

--- src/app/components/date-of-birth/date-of-birth.component.ts
+++ src/app/components/date-of-birth/date-of-birth.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-date-of-birth',
-  imports: [],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './date-of-birth.component.html',
   styleUrl: './date-of-birth.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DateOfBirthComponent {
-
-}
+export class DateOfBirthComponent {}
