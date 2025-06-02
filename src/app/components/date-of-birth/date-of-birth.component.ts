3343aa31c29932c2bc5cb4b6fba52c001917eac3
Maks <makzylinski@gmail.com>
2025-06-02 23:58:42 +0200

    added date of birth component

---
+++ src/app/components/date-of-birth/date-of-birth.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-of-birth',
+  imports: [],
+  templateUrl: './date-of-birth.component.html',
+  styleUrl: './date-of-birth.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DateOfBirthComponent {
+
+}
