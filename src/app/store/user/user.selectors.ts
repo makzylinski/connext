fea93b9bee5849cce53e9528b7774d41028b1889
Maks <makzylinski@gmail.com>
2025-06-09 23:15:26 +0200

    nobody reads this anyways

--- src/app/store/user/user.selectors.ts
+++ src/app/store/user/user.selectors.ts
@@ -2,6 +2,8 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { UserState } from './user-state.model';
 
 export const selectUserState = createFeatureSelector<UserState>('user');
+export const selectFirstLoginDataState =
+  createFeatureSelector<UserState>('firstLoginData');
 
 export const selectUser = createSelector(
   selectUserState,
@@ -11,4 +13,19 @@ export const selectUser = createSelector(
 export const selectUserId = createSelector(
   selectUserState,
   (state: UserState) => state.user?.id
+);
+
+export const selectFirstLoginPhotoUrl = createSelector(
+  selectFirstLoginDataState,
+  (state: UserState) => state.firstLoginData?.photoUrl
+);
+
+export const selectFirstLoginBio = createSelector(
+  selectFirstLoginDataState,
+  (state: UserState) => state.firstLoginData?.bio
+);
+
+export const selectFirstLoginDateOfBirth = createSelector(
+  selectFirstLoginDataState,
+  (state: UserState) => state.firstLoginData?.dateOfBirth
 );
